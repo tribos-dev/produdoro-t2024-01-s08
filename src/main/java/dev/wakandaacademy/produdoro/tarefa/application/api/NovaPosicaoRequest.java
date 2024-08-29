e0f0dee64841cc1426d393579d93d4936c8f4ec3
Joanerson Costa <joanersonc31@gmail.com>
2024-08-29 19:20:31 -0300

    fix: resolve bug construtor vazio nao encontrado na classe nova posicao request

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/NovaPosicaoRequest.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/NovaPosicaoRequest.java
@@ -4,9 +4,11 @@ import javax.validation.constraints.PositiveOrZero;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NovaPosicaoRequest {
 	@PositiveOrZero(message = "Posição deve ser maior ou igual a zero.")
 	private int novaPosicao;
